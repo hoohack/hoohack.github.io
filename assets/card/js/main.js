234a8f41b8bf9877dc9feaee65cafae2f34ab5cf
hhqcontinue <hhqcontinue@gmail.com>
2015-05-21 18:41:40 +0800

    add another small project

---
+++ assets/card/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+    var start = document.getElementById('start');
+    start.onmouseover = function() {
+        this.style.backgroundColor = '#333';
+    }
+
+    start.onmouseout = function() {
+        this.style.backgroundColor = '#fff';
+    }
+})();

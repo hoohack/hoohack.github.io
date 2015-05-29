622f101d0c93a622f83bd9ca357671f8d9acce36
hhqcontinue <hhqcontinue@gmail.com>
2015-05-29 09:55:02 +0800

    test

--- assets/card/js/page.js
+++ assets/card/js/page.js
@@ -2,15 +2,16 @@
 * @Author: Hector
 * @Date:   2015-05-22 17:15:52
 * @Last Modified by:   huhuaquan
-* @Last Modified time: 2015-05-28 12:44:02
+* @Last Modified time: 2015-05-28 17:14:05
 */
 (function() {
 
 	var card = (function() {
 		var first = 9,
 			second = 5.5,
-			animationProperty = 'webkitAnimationStart webkitAnimationEnd webkitAnimationIteration',
-			animateState = '-webkit-animation-play-state';
+			animationProperty = 'webkitAnimationEnd',
+			animateState = '-webkit-animation-play-state',
+			positions = [0, 190, 370, 550, 730, 910, 1090, 1270];
 
 		function move(begin, end, interval) {
 	    	var elem = $('#timeline'),
@@ -30,130 +31,116 @@
 		function drawTime(elemName) {
 			var elem = $('#' + elemName + '_time'),
 				top = 0,
-				height = 20;
+				height = 20,
+				interval = 10,
+				display = 1;
 			function frame() {
 				++top;
 				elem.height(top);
 				if (top == height) {
-					$('#' + elemName + '_desc').fadeTo('slow', 1);
+					$('#' + elemName + '_desc').fadeTo('slow', display);
 					clearInterval(id);
 				}
 			}
 
-			var id = setInterval(frame, 10);
+			var id = setInterval(frame, interval);
 		}
 
 		return {
 			init : function() {
 				var _this = this;
-				$('#egg').one(animationProperty, function(e) {
-					if ($(this).offset().left == 100) {
-						$(this).fadeTo('slow', 0);
-						$('#broegg').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/egg_broke.jpg)");
-						$(this).fadeTo('fast', 1);
-						drawTime('broegg');
-						setTimeout(function () {
-							_this.growToKid();
-							move(0, 180, first);
-						}, 500);
-					};
+				$('#broegg').addClass('animated bounceInDown').one(animationProperty, function(e) {
+					$(this).removeClass('bounceInDown');
+					$(this).css('background-image', "url(./images/egg_broke.jpg)");
+					drawTime('broegg');
+					setTimeout(function () {
+						_this.growToKid();
+						move(positions[0], positions[1], second);
+					}, 500);
 				});
 			},
-
 			growToKid : function() {
 				var _this = this;
-				$("#broegg").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 280) {
-						$(this).fadeTo('slow', 0);
-						$('#kid').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/kid.png)");
-						$(this).fadeTo('fast', 1);
-						$(this).height(107);
-						drawTime('kid');
-						setTimeout(function () {
-							_this.growToPupils();
-							move(181, 360, second);
-						}, 500);
-					};
-				});		
+				$("#kid").addClass('animated fadeInLeftBig').one(animationProperty, function(e) {
+					$(this).removeClass('fadeInLeftBig');
+					$(this).fadeTo('slow', 1);
+					drawTime('kid');
+					setTimeout(function () {
+						_this.growToPupils();
+						move(positions[1], positions[2], second);
+					}, 500);
+				});
 			},
 			growToPupils : function() {
 				var _this = this;
-				$("#kid").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 460) {
-						$(this).fadeTo('slow', 0);
-						$('#pupils').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/pupils.png)");
-						$(this).fadeTo('fast', 1);
-						drawTime('pupils');
-						setTimeout(function () {
-							_this.growToJunior();
-							move(361, 540, second);
-						}, 500);
-					};
+				$("#pupils").addClass('animated zoomInUp').one(animationProperty, function(e) {
+					$(this).removeClass('zoomInUp');
+					$(this).fadeTo('slow', 1);
+					drawTime('pupils');
+					setTimeout(function () {
+						_this.growToJunior();
+						move(positions[2], positions[3], second);
+					}, 500);
 				});
 			},
 			growToJunior : function() {
 				var _this = this;
-				$("#pupils").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 640) {
-						$(this).fadeTo('slow', 0);
-						$('#junior').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/junior.jpg)");
-						$(this).fadeTo('fast', 1);
-						drawTime('junior');
-						setTimeout(function () {
-							_this.growToSenior();
-							move(541, 720, second);
-						}, 500);
-					};
+				$("#junior").addClass('animated rollIn').one(animationProperty, function(e) {
+					$(this).removeClass('rollIn');
+					$(this).fadeTo('slow', 1);
+					drawTime('junior');
+					setTimeout(function () {
+						_this.growToWithme();
+						move(positions[3], positions[4], second);
+					}, 500);
+				});
+			},
+			growToWithme : function() {
+				var _this = this;
+				$("#withme").addClass('animated lightSpeedIn').one(animationProperty, function(e) {
+					$(this).removeClass('lightSpeedIn');
+					$(this).fadeTo('slow', 1);
+					drawTime('withme');
+					setTimeout(function () {
+						_this.growToSenior();
+						move(positions[4], positions[5], second);
+					}, 500);
 				});
 			},
 			growToSenior : function() {
 				var _this = this;
-				$("#junior").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 820) {
-						$(this).fadeTo('slow', 0);
-						$('#senior').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/senior.jpg)");
-						$(this).fadeTo('fast', 1);
-						drawTime('senior');
-						setTimeout(function () {
-							_this.growToCollege();
-							move(721, 900, second);
-						}, 500);
-					};
+				$("#senior").addClass('animated fadeInUpBig').one(animationProperty, function(e) {
+					$(this).removeClass('lightSpeedIn');
+					$(this).fadeTo('slow', 1);
+					drawTime('senior');
+					setTimeout(function () {
+						_this.growToCollege();
+						move(positions[5], positions[6], second);
+					}, 500);
 				});
 			},
 			growToCollege : function() {
 				var _this = this;
-				$("#senior").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 1000) {
-						$(this).fadeTo('slow', 0);
-						$('#college').fadeTo('slow', 1);
-						$(this).css('background-image', "url(./images/college.jpg)");
-						$(this).fadeTo('fast', 1);
-						drawTime('college');
-						setTimeout(function () {
-							_this.growToRecent();
-							move(901, 1080, second);
-						}, 500);
-					};
+				$("#college").addClass('animated fadeInDownBig').one(animationProperty, function(e) {
+					$(this).removeClass('fadeInDownBig');
+					$(this).fadeTo('slow', 1);
+					drawTime('college');
+					setTimeout(function () {
+						_this.growToRecent();
+						move(positions[6], positions[7], second);
+					}, 500);
 				});
 			},
 			growToRecent : function() {
-				$("#college").css(animateState, 'running').one(animationProperty, function(e) {
-					if ($(this).offset().left == 1180) {
-						$(this).fadeTo('slow', 0);
-						$('#recent').fadeTo('slow', 1);
-						drawTime('recent');
-					};
+				$("#recent").addClass('animated rotateIn').one(animationProperty, function(e) {
+					$(this).removeClass('rotateIn');
+					$(this).fadeTo('slow', 1);
+					drawTime('recent');
+					// $("#main-content").fadeOut(4000);
 				});
 			}
 
 		}
 	})();
-
 	card.init();
 })();
